0498a898eef20fb7793ab38f32ba758f3044ae24
unknown <jainrakshit389@gmail.com>
2025-06-04 15:50:24 +0530

    code updated

--- server/index.js
+++ server/index.js
@@ -13,10 +13,17 @@ connectDB()
   });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+     origin:process.env.FRONTEND_URL
+}));
 app.use(express.json());
 
 app.use("/api/jobs", require("./routes/jobsRoutes"));
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+}
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
